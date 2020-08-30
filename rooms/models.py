e7eca7d487a3ea5efcfff1ae2af0b7a4b981fc55
NicholasCF <nicholas.fernando@u.nus.edu>
2020-08-30 21:48:11 +0800

    Add room model

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    code = models.CharField(primary_key=True, max_length=32, blank=False)
+    name = models.CharField(max_length=256, blank=False)
+    address = models.CharField(max_length=256, blank=False)
+    max_cap = models.PositiveSmallIntegerField(blank=False)
