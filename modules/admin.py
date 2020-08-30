1f95828ac4baa247156016cc8f743551fa1b051c
NicholasCF <nicholas.fernando@u.nus.edu>
2020-08-30 19:01:12 +0800

    Add module model

--- modules/admin.py
+++ modules/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from modules.models import Module
+
+
+admin.site.register(Module)
--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Module(models.Model):
+    code = models.CharField(primary_key=True, max_length=32, blank=False)
+    name = models.CharField(max_length=256, blank=False)
+    description = models.TextField(blank=False)
+    time = models.DateTimeField(blank=True)
