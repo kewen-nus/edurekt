0ccf2b7306642b809c5c415994386bdee4b6f9d2
NicholasCF <nicholas.fernando@u.nus.edu>
2020-09-11 22:51:09 +0800

    Adjust model fields

--- modules/models.py
+++ modules/models.py
@@ -5,7 +5,7 @@ class Module(models.Model):
     code = models.CharField(primary_key=True, max_length=32, blank=False)
     name = models.CharField(max_length=256, blank=False)
     description = models.TextField(blank=False)
-    time = models.DateTimeField(blank=True)
+    time = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return "{}: {}".format(self.code, self.name)
--- rooms/models.py
+++ rooms/models.py
@@ -5,7 +5,7 @@ class Room(models.Model):
     code = models.CharField(primary_key=True, max_length=32, blank=False)
     name = models.CharField(max_length=256, blank=False)
     address = models.CharField(max_length=256, blank=False)
-    max_cap = models.PositiveSmallIntegerField(blank=False)
+    max_capacity = models.PositiveSmallIntegerField(blank=False)
 
     def __str__(self):
         return "{}: {}".format(self.code, self.name)
